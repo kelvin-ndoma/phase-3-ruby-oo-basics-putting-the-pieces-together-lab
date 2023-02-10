1563028ed0644d8d5502685882b6dc51dfe92999
kevin.mutua@student.moringaschool.com <kevin.mutua@student.moringaschool.com>
2023-02-10 14:42:28 +0300

    completed assignment

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,25 @@
 
+class Book
+    attr_accessor :title, :author, :page_count, :genre
+
+    def initialize(title)
+    @title = title
+    end
+
+    def turn_page
+        puts "Flipping the page...wow, you read fast!"
+    end
+    def info
+    puts "title: #{title} author: #{author} pages: #{page_count} genre: #{genre}"
+    end
+
+end
+
+
+book = Book.new("And Then There Were None")
+book.author = "Agatha Christie"
+book.page_count = 272
+book.genre = "Mystery"
+
+puts book.info
 
--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,1 +1,25 @@
 # Make your shoe class here!
+class Shoe
+    attr_accessor :brand, :color, :size, :material, :condition
+
+    def initialize(brand)
+        @brand=brand
+    end
+    def cobble
+        puts "Your shoe is as good as new!"
+        @condition = "new"
+    end
+    def info
+        puts "Brand: #{brand}, Color: #{color} Size:#{size} Material: #{material} Condition: #{condition} "
+    end
+end
+
+shoe =Shoe.new("Adidas")
+shoe.brand="Nike"
+shoe.color="red"
+shoe.size=9.5
+shoe.material="suede"
+shoe.condition="tattered"
+
+puts shoe.cobble
+puts shoe.info
